e205d690198ff53b35c1faa0c6c111a0d8833a31
iu <iu>
2021-12-01 16:50:06 +0800

    update 修复 复制bug

--- assets/js/copyCode.js
+++ assets/js/copyCode.js
@@ -7,20 +7,31 @@ codes.forEach((code) => {
     button.ariaLabel = "Copy code to clipboard"
     div.appendChild(button);
     code.parentNode.insertBefore(div, code);
-});
 
-const codeBlocks = codes.querySelectorAll('.rouge-code');
-const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+    const text = code.querySelector('.rouge-code').innerText;
 
-copyCodeButtons.forEach((copyCodeButton, index) => {
-    const code = codeBlocks[index].innerText;
-
-    copyCodeButton.addEventListener('click', () => {
-        window.navigator.clipboard.writeText(code);
-        copyCodeButton.classList.add('copied');
+    button.addEventListener('click', () => {
+        window.navigator.clipboard.writeText(text);
+        button.classList.add('copied');
 
         setTimeout(() => {
-            copyCodeButton.classList.remove('copied');
+            button.classList.remove('copied');
         }, 2000);
     });
 });
+
+// const codeBlocks = document.querySelectorAll('.rouge-code');
+// const copyCodeButtons = document.querySelectorAll('.copy-code-button');
+// console.log(codeBlocks)
+// copyCodeButtons.forEach((copyCodeButton, index) => {
+//     const code = codeBlocks[index].innerText;
+
+//     copyCodeButton.addEventListener('click', () => {
+//         window.navigator.clipboard.writeText(code);
+//         copyCodeButton.classList.add('copied');
+
+//         setTimeout(() => {
+//             copyCodeButton.classList.remove('copied');
+//         }, 2000);
+//     });
+// });
